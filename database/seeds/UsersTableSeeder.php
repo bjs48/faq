7223f36b1791130801ce4b85f4a463469d93f272
balaji <bjs48@njit.edu>
2018-04-07 18:07:07 -0400

    Created Users

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,10 +11,10 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+
         factory (App\User::class,50)->create()->each(function ($u) {
             //$u->posts()->save(factory(App\Post::class)->make());
         });
-
 
     }
 }
